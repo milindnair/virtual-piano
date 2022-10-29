524a52c30fecc1beb84b1ceb835a65cbb84f3a36
Milind Nair <milindnair261003@gmail.com>
2022-10-29 14:18:24 +0530

    added audio input features and styling

--- piano.js
+++ piano.js
@@ -29,3 +29,52 @@ function playNote(key) {
     key.classList.remove('active')
   })
 }
+
+const player = document.querySelector('.player_audio');
+const playerDisplay = document.querySelector('.player_display');
+
+const changeTempo = function () {
+  player.playbackRate = tempoInput.value;
+  tempoDisplay.textContent = tempoInput.value;
+};
+
+const changeSongURL = function (e) { 
+  let filename = player.src.split('\\').pop().split('/').pop();
+  playerDisplay.textContent = filename;
+}
+
+const changeSongFile = function (e) {
+  const target = e.currentTarget;
+  const file = target.files[0];
+  let reader;
+
+  if (target.files && file) {
+    reader = new FileReader();
+    reader.onload = function (e) {
+      player.setAttribute('src', e.target.result);
+      playerDisplay.textContent = file.name;
+      // player.play(); // auto play on load
+    }
+    reader.readAsDataURL(file);
+  }
+};
+
+// Or upload an MP3:
+const uploadInput = document.getElementById('upload_input');
+uploadInput.addEventListener('change', changeSongFile, false);
+
+
+// Playback tempo 
+const tempoInput = document.getElementById('tempo_input');
+tempoInput.addEventListener("change", changeTempo, false);
+const tempoDisplay = document.querySelector('.tempo_display');
+tempoDisplay.textContent = tempoInput.value;
+
+
+
+// Chords from:
+
+const player_chords = document.getElementById('player-chords');
+// player.src = urlInput.value;
+changeSongURL();
+changeTempo();
